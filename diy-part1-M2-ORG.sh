59bd17594cf79854c896fe81b1dfd759139d289a
tzpFox <tzp001@foxmail.com>
2026-04-14 17:54:57 +0800

    Update diy-part1-M2-ORG.sh

--- diy-part1-M2-ORG.sh
+++ diy-part1-M2-ORG.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >>feeds.conf.default
+echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >>feeds.conf.default
